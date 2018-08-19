84be1cf056d64bdb493f9e04801b2e1b4c5c8cae
Nate Collings <nate@natecollings.com>
2018-08-19 13:17:58 -0600

    Screen boundaries

--- src/objects/ship.ts
+++ src/objects/ship.ts
@@ -6,7 +6,7 @@ class Ship extends Phaser.GameObjects.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number, key: string) {
     super(scene, x, y, key);
     this.cursors = scene.input.keyboard.createCursorKeys();
-    this.speed = 100;
+    this.speed = 500;
   }
 
   update() {
@@ -15,19 +15,32 @@ class Ship extends Phaser.GameObjects.Sprite {
   }
 
   private getVelocity(): Phaser.Math.Vector2 {
+    let horizontal = 0;
+    let vertical = 0;
     let x = 0;
     let y = 0;
 
     if(this.cursors.left && this.cursors.left.isDown) {
-      x = -1 * this.speed;
+      horizontal = -1;
     } else if(this.cursors.right && this.cursors.right.isDown) {
-      x = this.speed;
+      horizontal = 1;
     }
 
     if(this.cursors.up && this.cursors.up.isDown) {
-      y = -1 * this.speed;
+      vertical = -1;
     } else if (this.cursors.down && this.cursors.down.isDown) {
-      y = 1 * this.speed;
+      vertical = 1;
+    }
+
+    const canMoveHorizontally = (horizontal < 0 && this.x >= 0) || (horizontal > 0 && this.x <= this.scene.sys.canvas.width);
+    const canMoveVertically = (vertical < 0 && this.y >= 0) || (vertical > 0 && this.y <= this.scene.sys.canvas.width);
+
+    if(canMoveHorizontally) {
+      x = horizontal * this.speed;
+    }
+
+    if(canMoveVertically) {
+      y = vertical * this.speed;
     }
 
     return new Phaser.Math.Vector2(x, y);
